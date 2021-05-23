57128f98e8774fb035d97636dda59f878bea3e77
mr-eyes <mabuelanin@gmail.com>
2021-05-23 23:58:27 +0200

    indexing script

---
+++ index.py
@@ -0,0 +1,20 @@
+import kProcessor as kp
+import sys
+
+kSize = 25
+
+if len(sys.argv) < 2:
+    sys.exit("run: python index.py <fasta_file> <kSize:default(25)>")
+
+elif len(sys.argv) == 2:
+    fasta_file = sys.argv[1]
+
+elif len(sys.argv) == 3:
+    fasta_file = sys.argv[1]
+    kSize = int(sys.argv[2])
+
+chunkSize = 5000
+
+KF = kp.kDataFrameMQF(kp.KMERS, kp.nonCanonicalInteger_Hasher, {"kSize":kSize})
+cKF = kp.index(KF, fasta_file, chunkSize, fasta_file + ".names")
+cKF.save("idx_" + fasta_file.replace(".fa",""))
